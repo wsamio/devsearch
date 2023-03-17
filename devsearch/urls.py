81897a704b0415ba343d5d2188806f385c377586
Iftu Bin Misbah <samio.cse@gmail.com>
2023-03-17 15:18:42 -0700

    update

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
-
-def projects(request):
-    return  HttpResponse('Here are our products')
-
-def project1(request, pk):
-    return  HttpResponse('SINGLE PROJECTS' + ' ' + str(pk))
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def projects(request):
+    return  HttpResponse('Here are our products')
+
+def project1(request, pk):
+    return  HttpResponse('SINGLE PROJECTS' + ' ' + str(pk))
