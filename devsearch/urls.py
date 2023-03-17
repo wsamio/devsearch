003a7335903cc4d055488fd880a8cba523c40a29
Iftu Bin Misbah <samio.cse@gmail.com>
2023-03-17 14:56:27 -0700

    update

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def projects(request):
+    return  HttpResponse('Here are our products')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
