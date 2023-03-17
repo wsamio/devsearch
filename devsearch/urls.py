6e73f661867a8633b7b4eaeda4c13f093719bdb6
Iftu Bin Misbah <samio.cse@gmail.com>
2023-03-17 15:05:50 -0700

    update

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -5,7 +5,11 @@ from django.http import HttpResponse
 def projects(request):
     return  HttpResponse('Here are our products')
 
+def project1(request):
+    return  HttpResponse('SINGLE PROJECTS')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', projects, name="projects"),
+    path('projects1/', project1, name="Single"),
 ]
