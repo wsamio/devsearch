411bc593cab6501f9a0156df01dbe62f671e3906
Iftu Bin Misbah <samio.cse@gmail.com>
2023-03-20 06:18:51 -0700

    update

--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(request):
-    return  render(request, 'projects/projects.html')
+    page = 'projects'
+    return  render(request, 'projects/projects.html', {'page': page})
 
 def project(request, pk):
     return  render(request, 'projects/single-project.html')
