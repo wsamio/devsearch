2ffeca86fc5d742f82690a506ad01b25397d691c
Iftu Bin Misbah <samio.cse@gmail.com>
2023-03-20 07:08:10 -0700

    update

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+project_list = [
+    {
+        'id' : '1',
+        'title' : "Ecommerce Website",
+        'description' : 'Fully functional eccomerce website'
+    },
+    {
+        'id' : '2',
+        'title' : "Portfolio Website",
+        'description' : 'This was a project where I built out my portfolio'
+
+    },
+]
+
 def projects(request):
     page = 'projects'
     number = 10
-    context = {'page' : page, 'number': number}
+    context = {'page' : page, 'number': number, 'projects' : project_list}
     return  render(request, 'projects/projects.html', context)
 
 def project(request, pk):
