ee353543e96770810b1951f1badc4280a63d2d25
Iftu Bin Misbah <samio.cse@gmail.com>
2023-03-20 05:10:47 -0700

    update

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    path('projects/', views.projects, name="projects"),
+    path('', views.projects, name="projects"),
     path('projects/<str:pk>/', views.project, name="Single"),
 ]
