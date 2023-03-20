6db354c1c913108e56610ed9a5de01e680b6be76
Iftu Bin Misbah <samio.cse@gmail.com>
2023-03-20 01:08:31 -0700

    update

--- projects/views.py
+++ projects/views.py
@@ -5,4 +5,4 @@ def projects(request):
     return  render(request, 'projects.html')
 
 def project1(request, pk):
-    return  HttpResponse('SINGLE PROJECTS' + ' ' + str(pk))
+    return  request(request, 'single-project.html')
