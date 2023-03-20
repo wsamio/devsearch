33a3dc74f47593256796433525230e1066b2fefb
Iftu Bin Misbah <samio.cse@gmail.com>
2023-03-20 15:44:05 -0700

    update

--- projects/models.py
+++ projects/models.py
@@ -9,6 +9,7 @@ class Project(models.Model):
     description = models.TextField(null=True, blank=True)
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToMany Field('Tag', blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
