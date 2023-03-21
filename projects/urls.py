dba981caa2b84dc5a0f8132f8ab2ecdf5802e02c
Iftu Bin Misbah <samio.cse@gmail.com>
2023-03-21 11:51:25 -0700

    update

--- projects/urls.py
+++ projects/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('', views.projects, name="projects"),
     path('project/<str:pk>/', views.project, name="project"),
     path('create-project/', views.createProject, name="create-project"),
+    path('update-project/<str:pk>/', views.UpdateProject, name="update-project"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -24,3 +24,15 @@ def createProject(request):
     context = {'form' : form}
     return render(request, 'projects/project_form.html', context)
 
+
+def UpdateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    
+    context = {'form' : form}
+    return render(request, 'projects/project_form.html', context)
