ec5649df963b563298c6e90022e7ec6ab618794e
Iftu Bin Misbah <samio.cse@gmail.com>
2023-03-22 09:30:40 -0700

    update

--- projects/forms.py
+++ projects/forms.py
@@ -1,7 +1,11 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 class ProjectForm(ModelForm):
     class Meta:
        model = Project
        fields = ['title', 'featured_image', 'description', 'demo_link', 'source_link', 'tags']
+       widgets = {
+            'tags':forms.CheckboxSelectMultiple(),
+       }
