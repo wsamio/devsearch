0159f4b11693824c8996793e68d8536932c53dde
Iftu Bin Misbah <samio.cse@gmail.com>
2023-03-22 06:48:41 -0700

    update

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -129,6 +129,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
