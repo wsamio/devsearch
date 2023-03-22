6419266682336fb1bd29d75410f8fd618cbe6b17
Iftu Bin Misbah <samio.cse@gmail.com>
2023-03-22 05:54:52 -0700

    update

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+MEDIA_URL= '/images/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
