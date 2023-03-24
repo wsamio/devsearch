2657acea5e3f99c7ec4fa97fb6a313c3139dabf8
Iftu Bin Misbah <samio.cse@gmail.com>
2023-03-24 01:58:45 -0700

    update

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('projects.urls')),
-    path('users/', include('users.urls')),
+    path('project/', include('projects.urls')),
+    path('', include('users.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
