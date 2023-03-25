7bfcd4a305d9881843cf75e69b489484282f4d6a
Iftu Bin Misbah <samio.cse@gmail.com>
2023-03-25 13:17:51 -0700

    update

--- users/models.py
+++ users/models.py
@@ -36,22 +36,3 @@ class Skill(models.Model):
 
     def __str__(self):
         return str(self.name)
-
-#@receiver(post_save, sender=Profile)
-def createProfile(sender, instance, created, **kwargs):
-    if created:
-        user = instance
-        profile = Profile.objects.create(
-            user=user,
-            username=user.username,
-            email = user.email,
-            name=user.first_name,
-        )
-
-
-def deleteUser(sender, instance, **kwargs):
-    user = instance.user
-    user.delete()
-
-post_save.connect(createProfile, sender=User)
-post_delete.connect(deleteUser, sender=Profile)
