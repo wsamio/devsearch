f79044975303940b818f2df2f5dc337d830a6324
Iftu Bin Misbah <samio.cse@gmail.com>
2023-03-25 15:04:32 -0700

    update

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
-
+from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
 
@@ -17,7 +17,7 @@ def loginUser(request):
         try:
             user = User.objects.get(username=username)
         except:
-            print('Username does not exist')
+            messages.error(request, 'Username does not exist')
 
         user = authenticate(request, username=username, password=password)
 
@@ -25,11 +25,12 @@ def loginUser(request):
             login(request, user)
             return redirect('profiles')
         else:
-            print('Username or password is incorrect')
+            messages.error(request, 'Username or password is incorrect')
     return render(request, 'users/login_register.html')
 
 def logoutUser(request):
     logout(request)
+    messages.error(request, 'User was logged out!')
     return redirect('login')
 
 def profiles(request):
