de160926df83bc34f2c8b26c443c2e40937bb82a
Iftu Bin Misbah <samio.cse@gmail.com>
2023-03-25 14:11:26 -0700

    update

--- users/views.py
+++ users/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 from .models import Profile
 
 # Create your views here.
 
-def loginPage(request):
+def loginUser(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -23,6 +23,10 @@ def loginPage(request):
         else:
             print('Username or password is incorrect')
     return render(request, 'users/login_register.html')
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
 def profiles(request):
     profiles = Profile.objects.all()
