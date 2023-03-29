aff9ec62f0f3792b47fb23657d77d1712c33d4e0
Iftu Bin Misbah <samio.cse@gmail.com>
2023-03-29 14:02:33 -0700

    update

--- projects/views.py
+++ projects/views.py
@@ -52,4 +52,4 @@ def DeleteProject(request, pk):
         project.delete()
         return redirect('projects')
     context = {'object' : project}
-    return render(request, 'projects/delete_template.html',  context)
+    return render(request, 'delete_template.html',  context)
