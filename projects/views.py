6bf75fb06de098a4fcdcc83ead519f5a714f31a5
Iftu Bin Misbah <samio.cse@gmail.com>
2023-03-29 13:00:01 -0700

    update

--- projects/views.py
+++ projects/views.py
@@ -15,11 +15,14 @@ def project(request, pk):
 
 @login_required(login_url="login")
 def createProject(request):
+    profile = request.user.profile
     form = ProjectForm()
     if request.method == 'POST':
         form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            project = form.save(commit=False)
+            project.owner = profile
+            project.save()
             return redirect('projects')
     
     context = {'form' : form}
@@ -28,7 +31,8 @@ def createProject(request):
 
 @login_required(login_url="login")
 def UpdateProject(request, pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
     form = ProjectForm(instance=project)
     if request.method == 'POST':
         form = ProjectForm(request.POST, request.FILES, instance=project)
