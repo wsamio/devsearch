a7b4929cb57130c7b976f7b551b4945ba571bad7
Iftu Bin Misbah <samio.cse@gmail.com>
2023-03-31 09:37:53 -0700

    update

--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Project
+from django.db.models import Q
+from .models import Project, Tag
 from .forms import ProjectForm
 
 def projects(request):
-    projects = Project.objects.all()
+    search_query = ''
+
+    if request.GET.get('search_query'):
+        search_query = request.GET.get('search_query')
+
+    projects = Project.objects.filter(Q(title__icontains=search_query) | Q(description__icontains=search_query) | Q(owner__name__icontains=search_query))
     context = {'projects' : projects}
     return render(request, 'projects/projects.html', context)
 
