420a600bf2f712c97e045368af6dd018446e60d6
Iftu Bin Misbah <samio.cse@gmail.com>
2023-04-02 09:07:12 -0700

    update

--- projects/models.py
+++ projects/models.py
@@ -21,7 +21,7 @@ class Project(models.Model):
         return self.title
 
     class Meta:
-        ordering = ['created']
+        ordering = ['-vote_ratio', '-vote_total', 'title']
 
     @property
     def getVoteCount(self):
