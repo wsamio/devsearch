082d11c1a867e0bab825f481b09df57e009bf77c
Iftu Bin Misbah <samio.cse@gmail.com>
2023-04-07 03:26:42 +0600

    update

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -121,6 +121,16 @@ USE_I18N = True
 USE_TZ = True
 
 
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'random@email.com'
+EMAIL_HOST_PASSWORD = 'asdfafsdefwasdf'
+
+
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
