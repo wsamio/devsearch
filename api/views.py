624e18fe8f2015c4cb3dce607df61d4728abcdbf
Iftu Bin Misbah <samio.cse@gmail.com>
2023-04-09 00:48:47 +0600

    update

--- api/views.py
+++ api/views.py
@@ -1,5 +1,8 @@
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+@api_view(['GET', 'POST'])
 def getRoutes(request):
 
     routes = [
