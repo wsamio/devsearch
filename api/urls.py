0ecb77bb6afe3b1d2716de16d56bfdb50f62d19c
Iftu Bin Misbah <samio.cse@gmail.com>
2023-04-09 01:40:40 +0600

    update

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoutes),
-    path('projects', views.getProject),
+    path('projects', views.getProjects),
+    path('projects/<str:pk>/', views.getProject),
 ]
--- api/views.py
+++ api/views.py
@@ -19,7 +19,13 @@ def getRoutes(request):
     return Response(routes)
 
 @api_view(['GET'])
-def getProject(request):
+def getProjects(request):
     projects = Project.objects.all()
     serializer = ProjectSerializer(projects, many=True) 
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getProject(request, pk):
+    project = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(project, many=False) 
     return Response(serializer.data)
