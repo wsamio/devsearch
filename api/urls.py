c9a524e097095fbb76ac01f6cbcb4d82860e28ba
Iftu Bin Misbah <samio.cse@gmail.com>
2023-04-11 21:28:48 +0600

    update

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoutes),
-    path('projects', views.getProjects),
+    path('projects/', views.getProjects),
     path('projects/<str:pk>/', views.getProject),
 ]
