c4d72515c2c78db4abc05627fdab6b005616caa1
Iftu Bin Misbah <samio.cse@gmail.com>
2023-05-01 02:52:49 +0600

    update

---
+++ frontend/login.js
@@ -0,0 +1,30 @@
+let form = document.getElementById('login-form')
+
+let tokenUrls = 'http://127.0.0.1:8000/api/users/token/'
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    let formData = {
+        'username': form.username.value,
+        'password': form.password.value
+    }
+    
+    fetch(tokenUrls, {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json',
+        },
+        body:JSON.stringify(formData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Data:', data.access)
+        if(data.access) {
+            localStorage.setItem('token', data.access)
+            window.location = 'file:///C:/Users/Acer/Desktop/frontend/projects-list.html'
+        } else {
+            alert('Username or password did not work!')
+        }
+    })
+})
